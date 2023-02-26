cb29a5b97fe323f79480e1ce3a81f69e79346d6f
jarvoren <jarvoren@gmail.com>
2023-02-26 06:44:02 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# WorldBuilder
+
+A Ue5 world procedural generator
+
+For any licencing/contributing/usage rights email jarvoren@gmail.com
+
+Developed with Unreal Engine 5
---
+++ Source/WorldBuilder/WorldMapGenerator.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "WorldMapGenerator.h"
+
+WorldMapGenerator::WorldMapGenerator()
+{
+}
+
+WorldMapGenerator::~WorldMapGenerator()
+{
+}
