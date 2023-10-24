287876d8eaabea969f100e3184a4b5bde7415a37
Jakub Dabek <jarvoren@gmail.com>
2023-10-24 22:10:36 +0200

    planing: add todos and headers for future development
    Signed-off-by: Jakub Dabek <jarvoren@gmail.com>

---
+++ WorldBuilderLib/include/generators/generator.h
@@ -0,0 +1,6 @@
+#ifndef H_GRID_GENERATOR_H
+#define H_GRID_GENERATOR_H
+
+/* Should GenerateTileset choosing a generator based on input */
+
+#endif // H_SQUARE_GRID_GENERATOR_H
--- WorldBuilderLib/src/world.cpp
+++ WorldBuilderLib/src/world.cpp
@@ -19,7 +19,17 @@ World::World(WorldConfig* cfg)
 {
 	/* Copy the config to the world object */
 	std::memcpy(&world_config, cfg, sizeof(WorldConfig));
+	
+	//TODO Generate basic tileset
 	tileset = GenerateTestTileset(world_config.grid_x_size, world_config.grid_y_size);
+
+	//TODO Populate the tileset with proper tiles - terrain only generation
+
+	//TODO Generate populace, main characters and factions
+
+	//TODO Populate the tiles with places (start of history)
+
+	//TODO Generate History of the world
 }
 
 
