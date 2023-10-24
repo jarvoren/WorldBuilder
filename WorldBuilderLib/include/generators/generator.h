ae314d2737754970c1d56f5d8eade86dc61d35ec
Jakub Dabek <jarvoren@gmail.com>
2023-10-24 22:41:15 +0200

    generator: add basis for factory
    Add base for generator to be a factory created object for the world.
    
    Signed-off-by: Jakub Dabek <jarvoren@gmail.com>

--- WorldBuilderLib/include/generators/generator.h
+++ WorldBuilderLib/include/generators/generator.h
@@ -1,6 +1,17 @@
 #ifndef H_GRID_GENERATOR_H
 #define H_GRID_GENERATOR_H
 
-/* Should GenerateTileset choosing a generator based on input */
+#include <grid_elements/tile.h>
+
+struct GeneratorConfig
+{
+
+};
+
+class Generator
+{
+public:
+	virtual Tile*** GenerateBasicTileset(GeneratorConfig cfg) = 0;
+};
 
 #endif // H_SQUARE_GRID_GENERATOR_H
